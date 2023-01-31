2e6484e078fdb4043ce015eb76177adcd9fb6440
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-01-31 21:35:53 +0100

    home page hamburger menu slide with JS

---
+++ Assets/JS/index.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#menu')
+
+document.querySelector('#flecheDroite').addEventListener('click', () => {
+  const elementsMenu = menu.querySelectorAll('.menu')
+  const last = elementsMenu[elementsMenu.length - 1]
+  last.remove()
+  document.querySelector('#flecheDroite').after(last)
+})
+
+
+document.querySelector('#flecheGauche').addEventListener('click', () => {
+  const elementsMenu = menu.querySelectorAll('.menu')
+  const first = elementsMenu[0]
+  first.remove()
+  document.querySelector('#flecheGauche').before(first)
+})
+
+
+
